c3b7c0e338b9081ee20005f47cd892f0e0ce5257
ribbinpo <ribbinpo@gmail.com>
2024-03-26 16:06:49 +0700

    feat: :sparkles: config read env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      envFilePath: ['.env', '.env.development.local'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
