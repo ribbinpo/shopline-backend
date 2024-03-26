7e9b5b65054400432193e2d0c87b00f7c5da6424
ribbinpo <ribbinpo@gmail.com>
2024-03-26 17:05:00 +0700

    feat: :sparkles: add class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
   const PORT = process.env.PORT || 4000;
   await app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,8 +1,16 @@
+import { IsEmail, IsEnum, IsNotEmpty } from 'class-validator';
 import { Role } from 'src/database/enums/role.enum';
 
-export interface CreateUserDto {
+export class CreateUserDto {
+  @IsNotEmpty()
   name: string;
+
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
   password: string;
+
+  @IsEnum(Role)
   role: Role;
 }
